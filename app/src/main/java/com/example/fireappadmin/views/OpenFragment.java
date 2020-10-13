80af69b9ab84aef62353d510277c2be0af228f85
ekbaya <Eliasbaya1223@gmail.com>
2020-10-13 22:00:48 +0300

    Initial comit

---
+++ app/src/main/java/com/example/fireappadmin/views/OpenFragment.java
@@ -0,0 +1,19 @@
+package com.example.fireappadmin.views;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.fireappadmin.R;
+
+public class OpenFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_open, container, false);
+        return root;
+    }
+}
